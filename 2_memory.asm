8226f0141196621181a9694fd4798199d21c2265
Vlad Anton <182820291+VladAnton22@users.noreply.github.com>
2025-10-03 22:43:42 +0100

    Did the second exercise

--- 2_memory.asm
+++ 2_memory.asm
@@ -1,2 +1,18 @@
 .data
-number
+numbers: .word 7, 6
+result: .word 0
+
+.text 
+main:
+	la $t0, numbers		# load address
+	lw $t1, 0($t0)		# load number1 into register
+	lw $t2, 4($t0)		# load number2 into register
+	add $t3, $t1, $t2
+	sw $t3, result		# save result into an address
+	
+	li $v0, 1
+	lw $a0, result
+	syscall
+	
+	li $v0, 10		#exit
+	syscall
